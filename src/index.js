dc7019f19dd32d3a8dbb372e21edde2d49c9c124
MarcosAdrianoGuedes <marcosadri55@hotmail.com>
2023-06-09 16:32:49 -0300

    alow deus

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Barbeiro from './pages/Barbeiro';
-import Home from './pages/Home';
-import ListaEspera from './pages/ListaEspera';
-import Recepcao from './pages/Recepcao';
-
 
 ReactDOM.render(
   <React.StrictMode>
