b0fa78b2737315fb072e13aeb41acab2ab101a6d
MarcosAdrianoGuedes <marcosadri55@hotmail.com>
2023-06-09 15:59:57 -0300

    testando

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Barbeiro from './pages/Barbeiro';
+import Home from './pages/Home';
+import ListaEspera from './pages/ListaEspera';
+import Recepcao from './pages/Recepcao';
+
 
 ReactDOM.render(
   <React.StrictMode>
